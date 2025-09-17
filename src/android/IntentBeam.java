61be3cb102a1d49b0f79c42dd3f383ae8d34abbb
Vinita6484 <85390301+Vinita6484@users.noreply.github.com>
2025-09-17 13:48:04 +0530

    Update IntentBeam.java

--- src/android/IntentBeam.java
+++ src/android/IntentBeam.java
@@ -5,18 +5,51 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import android.content.Intent;
 import android.net.Uri;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 
 public class IntentBeam extends CordovaPlugin {
+
   @Override
   public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
     if ("launchIntent".equals(action)) {
       String url = args.getString(0);
-      Intent intent = new Intent(Intent.ACTION_VIEW);
-      intent.setData(Uri.parse(url));
-      this.cordova.getActivity().startActivity(intent);
-      callbackContext.success();
+      showAlert("Launching: " + url); // Show alert before launching
+      launchIntent(url, callbackContext);
       return true;
     }
     return false;
+  }
+
+  private void launchIntent(String url, CallbackContext callbackContext) {
+    try {
+      Intent intent = new Intent(Intent.ACTION_VIEW);
+      intent.setData(Uri.parse(url));
+      Intent chooser = Intent.createChooser(intent, "Open with");
+      chooser.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+      this.cordova.getActivity().startActivity(chooser);
+      callbackContext.success();
+    } catch (Exception e) {
+      showAlert("Error: " + e.getMessage()); // Show alert on error
+      callbackContext.error("Failed to launch intent: " + e.getMessage());
+    }
+  }
+
+  private void showAlert(final String message) {
+    final CordovaInterface cordovaInterface = this.cordova;
+    cordova.getActivity().runOnUiThread(new Runnable() {
+      public void run() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(cordovaInterface.getActivity());
+        builder.setTitle("IntentBeam")
+               .setMessage(message)
+               .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                 public void onClick(DialogInterface dialog, int id) {
+                   dialog.dismiss();
+                 }
+               });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+      }
+    });
   }
 }
